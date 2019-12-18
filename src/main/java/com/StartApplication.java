396a2f5a2bd65d2d8ddd06a0cbd011e93e1dd797
kk16603827992@.com <@qwer789>
2019-12-18 14:46:45 +0800

    11111

--- src/main/java/com/StartApplication.java
+++ src/main/java/com/StartApplication.java
@@ -7,5 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class);
+
+        System.out.println();
+
     }
 }
